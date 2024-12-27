739350721c48ef86728766e2ed6e8d21773991dd
AxelBR16 <117136772+AxelBR16@users.noreply.github.com>
2024-12-27 11:26:42 -0600

    Initial commit

---
+++ src/main/java/com/ttorientacion/TtOrientacionApplication.java
@@ -0,0 +1,13 @@
+package com.ttorientacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtOrientacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtOrientacionApplication.class, args);
+    }
+
+}
